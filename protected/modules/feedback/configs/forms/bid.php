be6ca812019c87bde5d3823b60dd2376a0c1becc
Sergey <Lukichev_s@mail.ru>
2021-04-29 14:17:45 +0700

    Настройка конфига без радиобатанов и файлов

--- protected/modules/feedback/configs/forms/bid.php
+++ protected/modules/feedback/configs/forms/bid.php
@@ -1,1 +1,201 @@
 <?php
+/**
+ * Обратный звонок
+ *
+ * 1 Имя
+ * 3 Контактный телефон
+ */
+return array(
+    'bid' => array(
+        'title' => 'Анкета',
+        'short_title' => 'Анкеты',
+        // Options
+        'options' => array(
+            'useCaptcha' => false,
+            'sendMail' => true,
+            'emptyMessage' => 'Анкет нет',
+        ),
+        // Form attributes
+        'attributes' => array(
+            'name' => array(
+                'label' => 'Ваши Фамилия, Имя, Отчество',
+                'type' => 'String', // String, Phone, Text, Checkbox, List
+                'rules' => array(
+                    array('name', 'required')
+                ),
+            ),
+            'phone' => array(
+                'label' => 'Контактный телефон',
+                'type' => 'Phone',
+                'rules' => array(
+                    array('phone', 'required')
+                ),
+            ),
+            'messenger' => array(
+                'label' => 'Мессенджер',
+                'type' => 'String',
+                'rules' => array(
+                    array('messenger', 'required')
+                ),
+            ),
+            'wechat' => array(
+                'label' => 'WeChat',
+                'type' => 'String'
+            ),
+            'email' => array(
+                'label' => 'Мессенджер',
+                'type' => 'String',
+                'rules' => array(
+                    array('email', 'required')
+                ),
+            ),
+            'date' => array(
+                'label' => 'Ваша дата рождения',
+                'type' => 'date',
+                'rules' => array(
+                    array('date', 'required')
+                ),
+            ),
+            'education' => array(
+                'label' => 'Ваше образование',
+                'type' => 'String',
+                'rules' => array(
+                    array('education', 'required')
+                ),
+            ),
+            'specialty' => array(
+                'label' => 'Ваша специальность',
+                'type' => 'String',
+                'rules' => array(
+                    array('specialty', 'required')
+                ),
+            ),
+            'city' => array(
+                'label' => 'Город проживания',
+                'type' => 'String',
+                'rules' => array(
+                    array('city', 'required')
+                ),
+            ),
+            'job' => array(
+                'label' => 'Место работы/учебы',
+                'type' => 'String',
+                'rules' => array(
+                    array('job', 'required')
+                ),
+            ),
+            //Семейное положение
+            //Дети
+            'height' => array(
+                'label' => 'Ваш рост',
+                'type' => 'String',
+                'rules' => array(
+                    array('height', 'required')
+                ),
+            ),
+            'weight' => array(
+                'label' => 'Ваш вес',
+                'type' => 'String',
+                'rules' => array(
+                    array('weight', 'required')
+                ),
+            ),
+            'heir_color' => array(
+                'label' => 'Ваш цвет волос',
+                'type' => 'String',
+                'rules' => array(
+                    array('heir_color', 'required')
+                ),
+            ),
+            'hobby' => array(
+                'label' => 'Увлечения, хобби',
+                'type' => 'String',
+                'rules' => array(
+                    array('hobby', 'required')
+                ),
+            ),
+            'positive_feature' => array(
+                'label' => 'Ваши положительные черты',
+                'type' => 'String',
+                'rules' => array(
+                    array('positive_feature', 'required')
+                ),
+            ),
+            'negative_feature' => array(
+                'label' => 'Ваши отрицательные черты',
+                'type' => 'String',
+                'rules' => array(
+                    array('negative_feature', 'required')
+                ),
+            ),
+            'bad_habits' => array(
+                'label' => 'Вредные привычки',
+                'type' => 'String',
+                'rules' => array(
+                    array('bad_habits', 'required')
+                ),
+            ),
+            'foreign_languages' => array(
+                'label' => 'Занание иностранных языков и уровень знания',
+                'type' => 'String',
+                'rules' => array(
+                    array('foreign_languages', 'required')
+                ),
+            ),
+            'requirements' => array(
+                'label' => 'Требования к партнеру',
+                'type' => 'Text',
+                'rules' => array(
+                    array('requirements', 'required')
+                ),
+            ),
+            'foreigners' => array(
+                'label' => 'Знакомство с иностранцами',
+                'type' => 'String',
+                'rules' => array(
+                    array('foreigners', 'required')
+                ),
+            ),
+            'social_network' => array(
+                'label' => 'Соцсеть',
+                'type' => 'String',
+                'rules' => array(
+                    array('social_network', 'required')
+                ),
+            ),
+            //Предпочтительный способ связи
+//            'file1' => array(
+//                'type' => 'String',
+//                'rules' => array(
+//                    array('file1', 'required')
+//                ),
+//            ),
+//            'file2' => array(
+//                'type' => 'String',
+//                'rules' => array(
+//                    array('file2', 'required')
+//                ),
+//            ),
+//            'file3' => array(
+//                'type' => 'String',
+//                'rules' => array(
+//                    array('file3', 'required')
+//                ),
+//            ),
+            'privacy_policy' => array(
+                'label' => 'Нажимая на кнопку "Отправить", я даю согласие на ' . \CHtml::link('обработку персональных данных', ['/site/page', 'id'=>\D::cms('privacy_policy')], ['target'=>'_blank']),
+                'type' => 'Checkbox',
+                'rules' => array(
+                    array('privacy_policy', 'required')
+                ),
+                'htmlOptions'=>['class'=>'inpt inpt-privacy_policy']
+            ),
+        ),
+        // Control buttons
+        'controls' => array(
+            'send' => array(
+                'title' => 'Отправить'
+            ),
+        ),
+    ),
+);
--- protected/modules/feedback/widgets/views/bid.php
+++ protected/modules/feedback/widgets/views/bid.php
@@ -1,1 +1,86 @@
 <?php
+
+/** @var FeedbackWidget $this */
+/** @var FeedbackFactory $factory */
+
+use common\components\helpers\HYii as Y;
+
+Y::js(
+    'feedback' . $this->getHash(),
+    'var feedback' . $this->getHash() . '=FeedbackWidget.clone(FeedbackWidget);feedback' . $this->getHash() . '.init("' . $this->id . '");'
+);
+
+?>
+<div id="<?= $this->id; ?>" class="<?= $this->getOption('html', 'class'); ?>">
+    <?php
+    $form = $this->beginWidget('CActiveForm', [
+        'id' =>  $this->getFormId(),
+        'action' => $this->getFormAction(),
+        'enableClientValidation' => true,
+        'enableAjaxValidation' => true,
+        'clientOptions' => [
+            'validateOnSubmit' => true,
+            'validateOnChange' => false,
+            'afterValidate' => 'js:feedback' . $this->getHash() . '.afterValidate',
+        ],
+        'htmlOptions' => [
+            'class' => 'bid-form'
+        ]
+    ]);
+    $model = $factory->getModelFactory()->getModel();
+    $fields = $factory->getModelFactory()->getAttributes();
+
+    $labelName = $factory->getOption("attributes.name.label");
+    $labelPhone = $factory->getOption("attributes.phone.label");
+    $labelCommentary = $factory->getOption("attributes.commentary.label");
+    $labelPrivacy = $factory->getOption("attributes.privacy_policy.label");
+    ?>
+
+    <?= CHtml::hiddenField('formId', $this->getFormId()); ?>
+
+    <?php
+    if (is_callable($this->onBefore)) call_user_func($this->onBefore, $model);
+    ?>
+
+    <?php if ($this->title) : ?>
+        <div class="cbHead">
+            <p><?= $this->title; ?></p>
+        </div>
+    <?php endif; ?>
+
+    <div class="feedback-body fields">
+        <label class="label label-name">
+            <span><?= $labelName; ?></span>
+            <?= $fields['name']->getModel()->widget($factory, $form, $this->params) ?>
+        </label>
+
+        <label class="label label-phone">
+            <span><?= $labelPhone; ?></span>
+            <?= $fields['phone']->getModel()->widget($factory, $form, $this->params) ?>
+        </label>
+
+        <label class="label label-commentary">
+            <span><?= $labelCommentary; ?></span>
+            <?= $fields['commentary']->getModel()->widget($factory, $form, $this->params) ?>
+        </label>
+
+        <div class="bottom">
+            <div class="check">
+                <?= $fields['privacy_policy']->getModel()->widget($factory, $form, $this->params) ?>
+            </div>
+
+            <?php if ($factory->getModelFactory()->getModel()->useCaptcha) {
+                $this->widget('feedback.widgets.captcha.CaptchaWidget');
+            } ?>
+
+            <?= CHtml::submitButton($factory->getOption('controls.send.title', 'Отправить'), [
+                'class' => 'feedback-submit-button btn',
+                'id' => $factory->getModelFactory()->getModel()->recaptchaBehavior->attachReCaptchaBySubmitButton()
+            ]); ?>
+        </div>
+    </div>
+
+    <div class="feedback-footer"></div>
+
+    <?php $this->endWidget(); ?>
+</div>
