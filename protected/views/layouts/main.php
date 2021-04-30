6a7cbd1714e9cf05d0bdcb7e9f4620fc8d93249e
Dmitry <Yamshikov>
2021-04-30 10:29:29 +0700

    Готова главная страница

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,72 +1,90 @@
-<?php use common\components\helpers\HYii as Y; ?>
+<?php
+
+use common\components\helpers\HYii as Y; ?>
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
-    <?php
-    CmsHtml::head();
-    CmsHtml::js($this->template . '/js/bootstrap.min.js');
-    CmsHtml::js($this->template . '/js/jquery.mmenu.all.js');
-    CmsHtml::js($this->template . '/js/libs.js');
-    CmsHtml::js($this->template . '/js/script.js');
-    CmsHtml::js('/js/main.js');
-    CmsHtml::js($this->template . '/js/custom.js');
-    ?>
+  <?php
+  CmsHtml::head();
+  CmsHtml::js($this->template . '/js/bootstrap.min.js');
+  CmsHtml::js($this->template . '/js/jquery.mmenu.all.js');
+  CmsHtml::js($this->template . '/js/libs.js');
+  CmsHtml::js($this->template . '/js/script.js');
+  CmsHtml::js('/js/main.js');
+  CmsHtml::js($this->template . '/js/custom.js');
+  ?>
 
-    <?php if (\Yii::app()->params['isAdaptive']): ?>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <? else: ?>
-        <meta name="viewport" content="width=device-width">
-    <? endif; ?>
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <?php if (\Yii::app()->params['isAdaptive']) : ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <? else : ?>
+    <meta name="viewport" content="width=device-width">
+  <? endif; ?>
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="canonical" href="<?=$this->createAbsoluteUrl('/').preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI'])?>" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="canonical" href="<?= $this->createAbsoluteUrl('/') . preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']) ?>" />
 </head>
 
 <body class="<?= D::c($this->isIndex(), 'index-page', 'inner-page') ?>">
 
-<!-- Для мобильной версии есть два варианта шапки -->
-<!-- header-bottom--type-1 - Только телефеон -->
-<!-- header-bottom--type-2 - Телефон, поиск, иконки мессенжеров -->
+  <!-- Для мобильной версии есть два варианта шапки -->
+  <!-- header-bottom--type-1 - Только телефеон -->
+  <!-- header-bottom--type-2 - Телефон, поиск, иконки мессенжеров -->
 
-<div id="my-page">
+  <div id="my-page">
     <header id="my-header" class="header">
       <div class="container">
         <div class="header__wrapper">
           <div class="logo-slogan">
             <a href="/" class="logo">
-            <img src="/images/Logo.svg" alt="logo">
-          </a>
-          <div class="header__slogan">
-            <?= D::cms('slogan') ?>
-          </div>
+              <img src="/images/Logo.svg" alt="logo">
+            </a>
+            <div class="header__slogan">
+              <?= D::cms('slogan') ?>
+            </div>
           </div>
           <div class="menu">
-            <?php $this->widget('\menu\widgets\menu\MenuWidget', array( 'rootLimit' => D::cms('menu_limit'), 'cssClass' => '' )); ?>
-          </div>  
+            <?php $this->widget('\menu\widgets\menu\MenuWidget', array('rootLimit' => D::cms('menu_limit'), 'cssClass' => '')); ?>
+          </div>
         </div>
       </div>
     </header>
 
     <main id="my-content" class="content">
-      <?=$content?>
+      <?= $content ?>
     </main>
 
     <footer id="my-footer" class="footer">
-
-    </footer>
-</div>
-
-<div id="totop"><p>&#xe851;</p> ^ Наверх</div>
-
-<?php if (D::yd()->isActive('feedback')): // обратный звонок ?>
-    <div style="display: none;">
-        <div id="form-callback">
-            <div class="popup-info">
-                <?php $this->widget('\feedback\widgets\FeedbackWidget', array('id' => 'callback', 'title'=>'Заказать звонок')) ?>
-            </div>
+      <div class="container">
+        <div class="footer__wrapper">
+          <div class="footer__row">
+            <?php ModuleHelper::Copyright() ?>
+            <a href="/privacy-policy" class="privacy-policy" target="_blanck">Политика конфиденциальности</a>
+          </div>
+          <div class="footer__row">
+            <?= D::cms('privacy_policy_text') ?>
+          </div> 
         </div>
+      </div>
+    </footer>
+  </div>
+
+  <div id="totop">
+    <p>&#xe851;</p> ^ Наверх
+  </div>
+
+  <?php if (D::yd()->isActive('feedback')) : // обратный звонок 
+  ?>
+    <div style="display: none;">
+      <div id="form-callback">
+        <div class="popup-info">
+          <?php $this->widget('\feedback\widgets\FeedbackWidget', array('id' => 'callback', 'title' => 'Заказать звонок')) ?>
+        </div>
+      </div>
     </div>
-<?php endif; // обратный звонок ?>
+  <?php endif; // обратный звонок 
+  ?>
 </body>
+
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,19 +1,47 @@
-<?php foreach (IBHelper::getElements(1) as $element): ?>
-<div class="banner" style="background-image: url(<?=$element['preview']?>)">
-    <div class="banner__text">
-        <div class="banner__title"><?= $element[props][title]?></div>
-        <div class="banner__subtitle"><?= $element[props][subtitle]?></div>
+<?php foreach (IBHelper::getElements(1) as $element) : ?>
+    <div class="banner" style="background-image: url(<?= $element['preview'] ?>)">
+        <div class="banner__text">
+            <div class="banner__title"><?= $element['props']['title'] ?></div>
+            <div class="banner__subtitle"><?= $element['props']['subtitle'] ?></div>
+        </div>
     </div>
-</div>
-<?php  endforeach; ?>
+<?php endforeach; ?>
 <div class="page-content">
     <div class="page-content__container container">
-        <?/* \crud\models\ar\Service::widget(); */?>
+        <?/* \crud\models\ar\Service::widget(); */ ?>
         <?= $page->text ?>
     </div>
 </div>
 
-<section class="reasons">
-    
+<section class="reason">
+    <div class="container">
+        <?php $iblock = IBHelper::getIblockByPk(2); ?>
+        <div class="reason__title"><?= $iblock->title; ?></div>
+        <div class="reason__wrapper">
+            <?php foreach (IBHelper::getElements(2) as $element) : ?>
+                <div class="reason__item">
+                    <div class="reason__img">
+                        <img src="<?= $element['preview'] ?>" alt="img">
+                    </div>
+                    <div class="reason__decor"></div>
+                    <div class="reason__descr">
+                        <span><?= $element['description'] ?></span>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </section>
 
+<section class="toankete">
+    <div class="container">
+        <div class="toankete__wrapper">
+            <div class="toankete__img"><img src="/images/heart.png" alt="heart"></div>
+            <div class="toankete__text">
+                <div class="toankete__title">Хотите найти китайского мужа? Заполните нашу анкету</div>
+                <div class="toankete__subtitle">Это займёт всего несколько минут</div>
+                <a class='btn' href="/anketa">Хочу замуж!</a>
+            </div>
+        </div>
+    </div>
+</section>
